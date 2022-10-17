9133bbd90fecda067338b41af22c5560bb67e097
Igor <igor@gmail.com>
2022-10-17 15:52:28 +0300

    feat: fix queue

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -23,7 +23,7 @@ class Queue {
     return this.top;
   }
 
-  enqueue(/* value */) {
+  enqueue(value) {
     if (this.length) {
       let current = this.top;
       while (current.next) {
@@ -40,16 +40,13 @@ class Queue {
     const topValue = this.top.value;
     this.top = this.top.next;
     this.length--;
+
     return topValue;
   }
 }
 
-const queue = new Queue();
+// const queue = new Queue();
 
-queue.enqueue(1); // adds the element to the queue
-queue.enqueue(3); // adds the element to the queue
-queue.dequeue(); // returns the top element from queue and deletes it, returns 1
-queue.getUnderlyingList() // returns { value: 3, next: null }
 
 module.exports = {
   Queue
